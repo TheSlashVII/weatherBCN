fb0e4eda37a8a0fd979b06c6344b132edbbc2965
TheSlashVII <junioruzama@gmail.com>
2025-03-30 22:17:21 +0200

    Information fetch filtered (kinda)

--- src/fetchInfo.ts
+++ src/fetchInfo.ts
@@ -8,12 +8,13 @@
 * */
 // imports
 import {writeFile} from "fs";
+import {Ajv} from "ajv";
 
 
 
 // types (interfaces) para filtrar informacion
 export type relevantPredictionFilter = {
-    ciudades: {
+    ciudades:[{
         name: string,
         stateSky: {
             description: string,
@@ -22,12 +23,54 @@ export type relevantPredictionFilter = {
             max: string,
             min: string,
         }
+    }]  // esto lo hago porque necesitaba una lista de objetos de tipo ciudad
+}
+// information
+const info:relevantPredictionFilter = await fetch("https://www.el-tiempo.net/api/json/v2/provincias/08").then((res) => res.json());
+const filteredCities:relevantPredictionFilter[] = []
+
+
+info.ciudades.forEach(currentCity => {
+    let eCity:relevantPredictionFilter = {
+        ciudades: [{
+            name: currentCity.name,
+            stateSky: {
+                description: currentCity.stateSky.description,
+            },
+            temperatures: {
+                max: currentCity.temperatures.max,
+                min: currentCity.temperatures.min,
+            }
+        }]
+
+    }
+    filteredCities.push(eCity);
+})
+
+
+
+
+
+/*
+const schema = {
+    type: "object",
+    properties: {
+        ciudades: {
+            type: "array",
+
+
+        }
     }
 }
 
+const ajvObject = new Ajv();
+const validator = ajvObject.compile()
+*/
 const conf = {
-    JSON_File: "clean.json",
-    json: await fetch("https://www.el-tiempo.net/api/json/v2/provincias/08").then((res) => res.json()),
+    JSON_File: "./clean.json",
+    json: filteredCities,
 }
-// writeFile(conf.JSON_File, JSON.stringify(conf.json, null, 2),)
+
+writeFile(conf.JSON_File, JSON.stringify(conf, null, 2),() => {});
+
 
