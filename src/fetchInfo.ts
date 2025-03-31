b8c14c52d3ffe1d8b6ec307c596887b6204e9546
TheSlashVII <junioruzama@gmail.com>
2025-03-31 18:46:09 +0200

    Corrected validation schema / pre page setup

--- src/fetchInfo.ts
+++ src/fetchInfo.ts
@@ -7,12 +7,19 @@
 *
 * */
 // imports
-import {writeFile} from "fs";
-import {Ajv} from "ajv";
+import {writeFile} from "fs"; // Funcion utilizada como un remplazo de Bun.write (no funcionaba por alguna razon)
+import {Ajv} from "ajv"; // validador
 
 
 
 // types (interfaces) para filtrar informacion
+/*
+* Aqui abajpo hice que el camppo ciudades fuera una lista porque
+* en el JSON que recibimos nos da la informacion en forma de lista
+* en ese campo.
+*
+* Haciendo eso puedo iterar por todos los objetos
+* */
 export type relevantPredictionFilter = {
     ciudades:[{
         name: string,
@@ -25,12 +32,13 @@ export type relevantPredictionFilter = {
         }
     }]  // esto lo hago porque necesitaba una lista de objetos de tipo ciudad
 }
-// information
+// Response del json que buscamos
 const info:relevantPredictionFilter = await fetch("https://www.el-tiempo.net/api/json/v2/provincias/08").then((res) => res.json());
-const filteredCities:relevantPredictionFilter[] = []
+const filteredCities:relevantPredictionFilter[] = [] // inicializamos esta array vacia para luego meter los campos del json que queremos
 
 
 info.ciudades.forEach(currentCity => {
+    // creo un objeto que contenga los campos que necesito
     let eCity:relevantPredictionFilter = {
         ciudades: [{
             name: currentCity.name,
@@ -48,29 +56,82 @@ info.ciudades.forEach(currentCity => {
 })
 
 
+// configuracion de la funcion writefile
+const conf = {
+    JSON_File: "./clean.json", // ubicacion del fichero json
+    json: filteredCities, // informacion que pasaremos para escribir
+}
 
-
-
-/*
+// validacion del schema
 const schema = {
-    type: "object",
-    properties: {
-        ciudades: {
-            type: "array",
-
-
-        }
+    type: "array",
+    items: {
+        type: "object",
+        properties: {
+            ciudades: {
+                type: "array",
+                items: {
+                    type: "object",
+                    properties: {
+                        name: {
+                            type: "string"
+                        },
+                        stateSky: {
+                            type: "object",
+                            properties: {
+                                description: {
+                                    type: "string"
+                                }
+                            },
+                            required: [
+                                "description"
+                            ]
+                        },
+                        temperatures: {
+                            type: "object",
+                            properties: {
+                                max: {
+                                    type: "string"
+                                },
+                                min: {
+                                    type: "string"
+                                }
+                            },
+                            required: [
+                                "max",
+                                "min"
+                            ]
+                        }
+                    },
+                    required: [
+                        "name",
+                        "stateSky",
+                        "temperatures"
+                    ]
+                }
+            }
+        },
+        required: [
+            "ciudades"
+        ]
     }
 }
 
 const ajvObject = new Ajv();
-const validator = ajvObject.compile()
-*/
-const conf = {
-    JSON_File: "./clean.json",
-    json: filteredCities,
+const validator = ajvObject.compile(schema);
+if (validator(filteredCities)) {
+    // validamos si la informacion pasa la validacion
+    console.log("validation passed");
+    writeFile(conf.JSON_File, JSON.stringify(conf.json),() => {})
+} else {
+    // si no pasa mostramos un mensaje de error por pantalla
+    console.error("validation error");
 }
 
-writeFile(conf.JSON_File, JSON.stringify(conf, null, 2),() => {});
+
+
+
+
+// writeFile(conf.JSON_File, JSON.stringify(conf.json),() => {})
 
 
