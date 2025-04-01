fc5bd21d5b0b291621f16b5847a6a88dd9dbca75
TheSlashVII <junioruzama@gmail.com>
2025-04-01 11:17:31 +0200

    Version with dynamic options on the header

--- src/BarcelonaHeader.tsx
+++ src/BarcelonaHeader.tsx
@@ -1,12 +1,13 @@
 import './header.css';
-
+import {useState} from "react";
 
 
 function header() {
+    const [currentCity, setCurrentCity] = useState("Barcelona");
 
     return(
         <header>
-            <h2>Tiempo de Barcelona</h2>
+            <h2>Tiempo de {currentCity}</h2>
             <div className="provinces">
                 <h4>Tarragona</h4>
                 <h4>Lleida</h4>
