20c9a1eaed5fff9c76e278d9fb1ad4ff42bde1dc
TheSlashVII <junioruzama@gmail.com>
2025-04-02 17:01:41 +0200

    Minor modifications to the index.html file

--- src/commonHooks.ts
+++
@@ -1,5 +0,0 @@
-let city = "Barcelona"
-const availableCities:string[] = ["Barcelona", "Girona", "Tarragona", "Lleida"];
-let displayedOptions:string[] = availableCities.filter((currentCity) => currentCity.toLowerCase() !=city.toLowerCase());
-console.log(displayedOptions);
-
